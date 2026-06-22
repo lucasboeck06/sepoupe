5479c767118a848df7e2a9e6ec1b9e672bb175e9
Lucas Vieira Boeck Krobel <boeck@MacBook-Air-de-Lucas.local>
2026-06-22 10:38:56 -0300

    feat/Finalização da tratativa do payload do Asaas + inserção no banco

--- backend/src/database/webhookRepository.js
+++ backend/src/database/webhookRepository.js
@@ -11,16 +11,16 @@ export async function queryConsultaAsaasId(id) {
 
 export async function queryConsultaCategoria(destinatario) {
   const result = await pool.query(
-    "SELECT id FROM public.categorias WHERE nome = $1",
+    "SELECT id, nome FROM public.categorias WHERE ref_categoria = $1",
     [destinatario],
   );
 
-  return result.rows[0]?.id;
+  return result.rows[0];
 }
 
 export async function queryPayloadAsaas(transacao) {
   const result = await pool.query(
-    "INSERT INTO public.transacoes(usuario_id, descricao, categoria_id, tipo, valor, criado_em, operacao_tipo) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+    "INSERT INTO public.transacoes(usuario_id, descricao, categoria_id, tipo, valor, criado_em, info_reconhecidas, operacao_tipo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
     [
       transacao.usuario_id,
       transacao.descricao,
@@ -28,6 +28,7 @@ export async function queryPayloadAsaas(transacao) {
       transacao.tipo,
       transacao.valor,
       transacao.dataHora,
+      transacao.info_reconhecidas,
       transacao.operacao_tipo,
     ],
   );
--- backend/src/services/webhookService.js
+++ backend/src/services/webhookService.js
@@ -7,17 +7,20 @@ import {
 export async function criarTransacao(payload) {
   const usuarioId = await queryConsultaAsaasId(payload.account.id);
 
-  const categoriaId = await queryConsultaCategoria(
+  const categoria = await queryConsultaCategoria(
     payload.transfer.bankAccount.ownerName,
   );
 
+  const infoReconhecidas = !!(usuarioId && categoria); // Regra verdade, somente truthy se os dois corresponderem
+
   const transacao = {
     usuario_id: usuarioId,
-    descricao: payload.transfer.bankAccount.ownerName,
-    categoria_id: categoriaId,
+    descricao: categoria?.nome || null, // Puxa o nome da categoria, se for undefined, vira null
+    categoria_id: categoria?.id || null,
     tipo: "saida",
     valor: payload.transfer.value,
     dataHora: payload.dateCreated,
+    info_reconhecidas: infoReconhecidas,
     operacao_tipo: payload.transfer.operationType,
   };
 
