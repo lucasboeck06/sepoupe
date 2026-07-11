9ffd7aba89112829a0fee4ba50855ad690b7f6e9
Lucas Vieira Boeck Krobel <boeck@MacBook-Air-de-Lucas.local>
2026-07-11 13:59:36 -0300

    Div inicial de apresentação do dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,31 @@
 import NavBar from "../components/NavBar.jsx";
-// import RankGastos from "../components/RankGastos.jsx";
-// import GraficoGastosScroll from "../components/GraficoGastosScroll.jsx";
+import RankGastos from "../components/RankGastos.jsx";
+import GraficoGastosScroll from "../components/GraficoGastosScroll.jsx";
+import { Bell } from "lucide-react";
 
 export default function Dashboard() {
   return (
-    // Aqui o bg é F0F0F7
-    <div className="h-full flex flex-col justify-between bg-[#000000]">
-      {/* <div className="flex flex-col p-6 mb-18">
-        <div>
-          <h1>Total gasto esse mês:</h1>
-          <p>R$ 500,00</p>
-          <h2>Saldo disponível: R$ 1.000,00</h2>
+    <div className="flex flex-col justify-between bg-[#F0F0F7]">
+      <div className="flex flex-col p-6 mb-18 gap-4">
+        <div className="flex flex-row justify-between items-center">
+          <div>
+            <h1>
+              <span className="text-sm text-[#aeaeb5] font-light">
+                Boa tarde,
+              </span>
+              <br />
+              <span className="text-xl text-[#1a1a3b] font-semibold">
+                Lucas 👋
+              </span>
+            </h1>
+          </div>
+
+          <div className="bg-[#e1e1f7] p-3 rounded-full">
+            <Bell className="w-4 h-4 text-[#7a7aff]" />
+          </div>
         </div>
+
+        <div></div>
 
         <div>
           <h2>Top de gastos:</h2>
@@ -23,7 +37,7 @@ export default function Dashboard() {
         </div>
 
         <GraficoGastosScroll />
-      </div> */}
+      </div>
 
       <NavBar />
     </div>
