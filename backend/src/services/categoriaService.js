fc1ac7bec63fbf8a7dbb6e5b9618af32abe084fa
Lucas Vieira Boeck Krobel <boeck@MacBook-Air-de-Lucas.local>
2026-07-29 16:26:53 -0300

    Validação de conflito entre categorias

--- backend/src/services/categoriaService.js
+++ backend/src/services/categoriaService.js
@@ -6,24 +6,36 @@ import {
 } from "../database/categoriaRepository.js";
 
 export async function criaCategoria(nome, tipo) {
-  const result = await criaCategoriaQuery(nome, tipo);
+  const categoriaExistente = await consultaCategoriaPorNome(nome);
 
-  return result;
+  if (categoriaExistente) {
+    throw new Error("Já existe uma categoria com este nome!s");
+  }
+
+  return await criaCategoriaQuery(nome, tipo);
 }
 
 export async function listarCategorias() {
-  const result = await listaCategoriasQuery();
-  return result;
+  // Services mensageiro, passa func direto no return
+  return await listaCategoriasQuery();
 }
 
 export async function atualizaCategoria(nome, tipo, id) {
-  const result = await atualizaCategoriaQuery(nome, tipo, id);
+  const categoriaAtualizada = await atualizaCategoriaQuery(nome, tipo, id);
 
-  return result;
+  if (!categoriaAtualizada) {
+    throw new Error("Categoria não existe!");
+  }
+
+  return categoriaAtualizada;
 }
 
 export async function deletaCategoria(id) {
-  const result = deletaCategoriaQuery(id);
+  const categoriaDeletada = await deletaCategoriaQuery(id);
 
-  return result;
+  if (!categoriaDeletada) {
+    throw new Error("Categoria não existente");
+  }
+
+  return categoriaDeletada;
 }
