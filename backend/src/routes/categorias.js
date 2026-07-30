2d408cf3a327327d03497e5d53666533f0781eaf
Lucas Vieira Boeck Krobel <boeck@MacBook-Air-de-Lucas.local>
2026-07-30 14:13:16 -0300

    Refinamento do modelo de Routes + Routes das catgorias

--- backend/src/routes/categorias.js
+++ backend/src/routes/categorias.js
@@ -1,5 +1,21 @@
-import { listar } from "../controllers/categoriaController.js";
+import {
+  criar,
+  listar,
+  atualizar,
+  deletar,
+} from "../controllers/categoriaController.js";
 
-export function categoriasRoutes(fastify) {
-  fastify.get("/categorias", listar);
+export async function categoriasRoutes(fastify) {
+  // Se você tivesse uma rota pública (ex: listar categorias para visitantes verem),
+  // ela ficaria solta aqui fora, sem proteção.
+  // fastify.get('/categorias/publicas', listar);
+
+  fastify.register(async function rotasProtegidas(instanciaIsolada) {
+    instanciaIsolada.addHook("preHandler", verificarToken);
+
+    instanciaIsolada.post("/categorias", criar);
+    instanciaIsolada.get("/categorias", listar);
+    instanciaIsolada.put("/categorias", atualizar);
+    instanciaIsolada.delete("/categorias", deletar);
+  });
 }
