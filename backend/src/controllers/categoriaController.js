4675ee036ecdc5c59d59d75b30e2f0133ea6a686
Lucas Vieira Boeck Krobel <boeck@MacBook-Air-de-Lucas-2.local>
2026-08-07 16:23:21 -0300

    Correções de responsabilidade nas camadas das categorias, concluída

--- backend/src/controllers/categoriaController.js
+++ backend/src/controllers/categoriaController.js
@@ -9,10 +9,6 @@ export async function criar(request, reply) {
   try {
     const { nome, tipo } = request.body;
 
-    if (!nome || !tipo) {
-      return reply.status(400).send({ erro: "Os campos são obrigatóios!" });
-    }
-
     const novaCategoria = await criarCategoria(nome, tipo);
 
     return reply.status(201).send(novaCategoria);
@@ -25,10 +21,6 @@ export async function listar(request, reply) {
   try {
     const { nome } = request.query;
 
-    // nome é verdadeiro ? = sim, : = não
-    // previne espaços inesperados na digitação do usuário
-    const nomeLimpo = nome ? nome.trim() : undefined;
-
     const categorias = await listarCategorias(nomeLimpo);
     reply.status(200).send(categorias);
   } catch (err) {
@@ -39,10 +31,6 @@ export async function listar(request, reply) {
 export async function atualizar(request, reply) {
   try {
     const { nome, tipo, id } = request.body;
-
-    if ((!nome && !tipo) || !id) {
-      return reply.status(400).send({ erro: "Os campos são obrigatórios!" });
-    }
 
     const categoriaAtualizada = await atualizarCategoria(nome, tipo, id);
 
@@ -55,10 +43,6 @@ export async function atualizar(request, reply) {
 export async function deletar(request, reply) {
   try {
     const { id } = request.body;
-
-    if (!id) {
-      return reply.status(400).send({ erro: "O campo é obrigatório!" });
-    }
 
     const categoriaDeletada = await deletarCategoria(id);
 
