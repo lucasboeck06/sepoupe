e030e6fdab222a1f77fba5c5235744d44841ffc4
Lucas Vieira Boeck Krobel <boeck@MacBook-Air-de-Lucas-2.local>
2026-08-12 12:16:48 -0300

    Tela de NovaTransação finalizada

--- frontend/src/pages/NovaTransacao.jsx
+++ frontend/src/pages/NovaTransacao.jsx
@@ -1,20 +1,44 @@
 import { useEffect, useState, useRef } from "react";
-import { ChevronLeft, Search } from "lucide-react";
+import { ChevronLeft, Search, Plus } from "lucide-react";
 import { DynamicIcon } from "lucide-react/dynamic";
 
 export default function NovaTransacao() {
   const [centavos, setCentavos] = useState(0);
   const [descricao, setDescricao] = useState("");
   const inputRef = useRef(null);
+
+  const [busca, setBusca] = useState("");
   const [categorias, setCategorias] = useState([]);
+
+  // Função padrão de matar acentos e retornar somente o texto puro
+  function semAcento(texto) {
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+
+  const visiveis = busca
+    ? // Filter é usado para filtrar os elementos
+      // Nós usamos semAcento e toLower para deixar tudo cru e comparar
+      // (categoria da vez) => recebe categoria limpa e sem acento, comparada com o digitado limpo e sem acento
+      categorias
+        .filter((c) =>
+          semAcento(c.nome.toLowerCase()).includes(
+            semAcento(busca.toLowerCase()),
+          ),
+        )
+        .slice(0, 7)
+    : // Limita o início em 7 categorias (caso não haja busca)
+      categorias.slice(0, 7);
+
   const [categoriaId, setCategoriaId] = useState(null);
   const [data, setData] = useState(new Date().toISOString().slice(0, 10));
 
+  // No campo do valor, substitui tudo que não é dígito, por nada
   function handleValor(e) {
     const digitos = e.target.value.replace(/\D/g, "");
     setCentavos(Number(digitos));
   }
 
+  // Formata o insert para o modelo braasileiro e trazendo o efeito de direita para esquerda
   const valorFormatado = (centavos / 100).toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
@@ -38,7 +62,7 @@ export default function NovaTransacao() {
   }, []);
 
   return (
-    <div className="p-4 bg-white">
+    <div className="p-4 bg-white min-h-screen">
       <div className="flex flex-row items-center gap-2">
         <ChevronLeft color="#2c2438" size={18} />
         <h1 className="text-[#2c2438] font-semibold text-md">Nova Transacao</h1>
@@ -46,6 +70,7 @@ export default function NovaTransacao() {
 
       <p className="mt-4 mb-2 text-[#9c93a9] text-xs font-medium">Valor</p>
       <input
+        inputMode="numeric"
         className="border-2 border-[#f0eef6] focus:border-[#ece6f7] rounded-2xl p-3 w-full font-black text-[#2c2438] outline-none"
         value={valorFormatado}
         onChange={handleValor}
@@ -53,6 +78,9 @@ export default function NovaTransacao() {
 
       <p className="mt-3 mb-2 text-[#9c93a9] text-xs font-medium">Descrição</p>
       <input
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
         className="border-2 border-[#f0eef6] focus:border-[#ece6f7] rounded-2xl px-3 py-2 w-full text-md text-[0.90rem] text-[#2c2438] font-normal outline-none placeholder:text-xs placeholder:text-[#b0a9bd]"
         placeholder="Ex: Almoço no shopping"
         value={descricao}
@@ -70,18 +98,34 @@ export default function NovaTransacao() {
         <input
           ref={inputRef}
           placeholder="Pesquise a categoria"
+          value={busca}
+          onChange={(e) => setBusca(e.target.value)}
           className="w-full bg-transparent outline-none text-[0.90rem] text-[#2c2438] font-normal placeholder:text-xs placeholder:text-[#b0a9bd]"
         />
       </div>
 
-      <div className="grid grid-cols-4 gap-x-3 items-start mt-3">
-        {categorias.map((cat) => (
+      <div className="grid grid-cols-4 gap-x-3 items-start mt-3 h-35">
+        <button
+          type="button"
+          className={`flex flex-col justify-center items-center gap-1 py-2`}
+        >
+          <span
+            className={`border border-dashed border-[#d8d2e7] w-10 h-10 rounded-xl flex items-center justify-center bg-[#F4F2F7]`}
+          >
+            <Plus className="text-[#8c7bc7]" size={18} />
+          </span>
+          <span className="text-[0.70rem] text-center leading-tight">
+            Adicionar
+          </span>
+        </button>
+
+        {visiveis.map((cat) => (
           <button
             key={cat.id}
             type="button"
             // Cada volta do map cria um arrow function próprio para a categoria!
             onClick={() => setCategoriaId(cat.id)}
-            className={`flex flex-col justify-center items-center gap-1 py-2 ${categoriaId === cat.id ? "ring-2 border-purple-500" : ""}`}
+            className={`flex flex-col justify-center items-center gap-1 py-2 ${categoriaId === cat.id ? "bg-[#FAFAFA] rounded-2xl transition-colors" : ""}`}
           >
             <span
               className={`w-10 h-10 rounded-xl flex items-center justify-center`}
@@ -93,7 +137,7 @@ export default function NovaTransacao() {
                 size={16}
               />
             </span>
-            <span className="text-[0.55rem] text-center leading-tight">
+            <span className="text-[0.70rem] text-center leading-tight">
               {cat.nome}
             </span>
           </button>
@@ -108,7 +152,7 @@ export default function NovaTransacao() {
         onChange={(e) => setData(e.target.value)}
       />
 
-      <button className="mt-6 bg-[#8B7BC7] text-white text-[0.80rem] font-semibold rounded-2xl p-3 w-full">
+      <button className="mt-6 bg-[#8B7BC7] text-white text-[0.90rem] font-semibold rounded-2xl p-4 w-full">
         Criar transação
       </button>
     </div>
