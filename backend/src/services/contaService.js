6ec8d031fa35a1a77cd2029bfe5686bd8a3c7dc2
Lucas Vieira Boeck Krobel <boeck@MacBook-Air-de-Lucas-2.local>
2026-09-22 15:13:58 -0300

    Adição do comportamento de Investientos (apenas com poupança) no contasService

--- backend/src/services/contaService.js
+++ backend/src/services/contaService.js
@@ -61,3 +61,65 @@ export async function reverterMovimentacao(
       await contasRepository.adicionarSaldo("conta_corrente", valor);
   }
 }
+
+export async function registrarInvestimento(valorInvestido) {
+  if (!valorInvestido || valorInvestido <= 0) {
+    throw new Error(
+      "É necessário um valor para investir, e que seja maior que zero!",
+    );
+  }
+
+  // Abre uma conexão com o banco (tem um limite, por isso ela finally, acaba)
+  const client = await pool.connect();
+
+  // Por que usar try/catch? O que é isso?
+  // BEGIN/COMMIT/ROLLBACK serve para que, ou tudo seja feito ou nada! Garantindo a atomicidade dos dados
+  // O banco pode cair, o cliente pode perder sinal, e se isso ocorrer bem no meio de uma operação, uma das chamadas pode ser realizada e a outra não, e isso é inadimissível!
+  try {
+    // Começa o bloco
+    await client.query("BEGIN");
+    // Executa as duas chamadas
+    await contasRepository.reduzirSaldo(
+      "conta_corrente",
+      valorInvestido,
+      client,
+    );
+    // Falhou aqui? Cai no catch, ele executou o primeiro, opa!
+    await contasRepository.adicionarSaldo("poupanca", valorInvestido, client);
+    // Finaliza como uma coisa só!
+    await client.query("COMMIT");
+  } catch (erro) {
+    // Aqui a gente reverte a ação, caso haja qualquer erro, mitigando a passagem de uma única chamada
+    await client.query("ROLLBACK");
+    throw erro;
+    // Finaliza a conexão que foi aberta
+  } finally {
+    client.release();
+  }
+}
+
+export async function registrarResgate(valorResgatado) {
+  if (!valorResgatado || valorResgatado <= 0) {
+    throw new Error(
+      "É necessário um valor para resgatar, e que seja maior que zero!",
+    );
+  }
+
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+    await contasRepository.reduzirSaldo("poupanca", valorResgatado, client);
+    await contasRepository.adicionarSaldo(
+      "conta_corrente",
+      valorResgatado,
+      client,
+    );
+    await client.query("COMMIT");
+  } catch (erro) {
+    await client.query("ROLLBACK");
+    throw erro;
+  } finally {
+    client.release();
+  }
+}
